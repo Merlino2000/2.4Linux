e7adf5ea82f91deaedd1dae39d1eb923e557218c
Merlino2000 <56533369+Merlino2000@users.noreply.github.com>
2021-06-14 20:23:40 +0200

    1.0
    Initial scrips

---
+++ Scripts/MoveNagiosConfig.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+for dir in /var/cache/salt/master/minions/*; do
+  salt-cp main03-mv-404160.internal.cloudapp.net "$dir"/files/tmp/config.cfg /usr/local/nagios/etc/servers/$(basename "$dir").cfg
+done
+salt main03-mv-404160.internal.cloudapp.net cmd.run systemctl restart nagios.service
